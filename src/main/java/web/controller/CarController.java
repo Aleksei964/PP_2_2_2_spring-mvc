78a391d78a1332f8554041ee8bdeb3b9c65ba291
Aleksei964 <igo-anikeev@yandex.ru>
2024-01-22 21:27:33 +0400

    PP_2_2_2 исправлена

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,24 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarServiceImpl;
-
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carService;
-    public CarController(CarServiceImpl carService) {
+    private final CarService carService;
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping(value = "/cars")
     public String viewCars(@RequestParam(defaultValue = "5") int count, ModelMap model) {
-        List<Car> cars = carService.getCarsList(count);
-        model.addAttribute("cars", cars);
+        ModelMap cars = model.addAttribute("cars", carService.getCarsList(count));
         return "cars";
     }
-
-
 }
